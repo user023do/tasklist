bb9046186eccc6824fdda3f06a7f63467ea737dd
hideaki.ogino <2023dounyukenshu.023@gmail.com>
2023-05-30 10:03:07 +0900

    最終

---
+++ src/models/validators/KadaiValidator.java
@@ -0,0 +1,31 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class KadaiValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Task m) {
+        List<String> errors = new ArrayList<String>();
+
+        String content_error = validateContent(m.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // タイトルの必須入力チェック
+
+    // メッセージの必須入力チェック
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "メッセージを入力してください。";
+        }
+
+        return "";
+    }
+}
